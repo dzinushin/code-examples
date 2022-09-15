1ff60b72cd677ff1d7dc37c090ad628d9d895b56
Dmitry Zinushin <zinushin@gmail.com>
2022-09-15 17:51:10 +0300

    improvement

--- kotlin/kotlin-logging/src/main/kotlin/Main.kt
+++ kotlin/kotlin-logging/src/main/kotlin/Main.kt
@@ -6,7 +6,7 @@ fun main(args: Array<String>) {
 
     // Try adding program arguments via Run/Debug configuration.
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    log.info {  "Program arguments: ${args.joinToString()}" }
 
     try {
         val pi = 3.1415
