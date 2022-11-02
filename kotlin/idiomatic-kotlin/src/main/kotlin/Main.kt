21fb4b9b5336ae605bc36fb8b7e144e9d7a507db
Dmitry Zinushin <zinushin@gmail.com>
2022-11-02 10:12:16 +0300

    idiomatic-kotlin: lambdas.kt

--- kotlin/idiomatic-kotlin/src/main/kotlin/Main.kt
+++ kotlin/idiomatic-kotlin/src/main/kotlin/Main.kt
@@ -6,6 +6,7 @@ fun main(args: Array<String>) {
     rangesDemo()
     scopeFunctionsDemo()
     enumDemo()
+    lambdasDemo()
 }
 
 
---
+++ kotlin/idiomatic-kotlin/src/main/kotlin/lambdas.kt
@@ -0,0 +1,19 @@
+data class Person(val name: String, val age:Int)
+
+fun lambdasDemo() {
+    val sum = { x: Int, y: Int -> x + y }
+    println("sum 1, 2: ${sum(1,2)}");
+    { println(42) }()
+
+    run {
+        println(42)
+    }
+
+    val persons = listOf(Person("Young", 21), Person("Old", 31))
+
+    println("Oldest person: ${persons.maxBy({p: Person -> p.age})}")
+    println("Oldest person: ${persons.maxBy() { p: Person -> p.age } }")
+    println("Oldest person: ${persons.maxBy( { it.age } ) }")
+    println("Oldest person: ${persons.maxBy { it.age }}")
+    println("Oldest person: ${persons.maxBy(Person::age)}")
+}
