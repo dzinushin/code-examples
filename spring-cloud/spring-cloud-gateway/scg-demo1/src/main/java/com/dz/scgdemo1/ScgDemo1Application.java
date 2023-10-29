308ff63e7c48b786000871f8261bfc379d981a72
Dmitry Zinushin <zinushin@gmail.com>
2023-10-29 23:31:01 +0300

    add staff

---
+++ spring-cloud/spring-cloud-gateway/scg-demo1/src/main/java/com/dz/scgdemo1/ScgDemo1Application.java
@@ -0,0 +1,13 @@
+package com.dz.scgdemo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScgDemo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScgDemo1Application.class, args);
+	}
+
+}
---
+++ spring-cloud/spring-cloud-gateway/scg-demo1/src/test/java/com/dz/scgdemo1/ScgDemo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dz.scgdemo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScgDemo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
