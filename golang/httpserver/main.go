0a46bf29585b7a0aca4b21ece918da07551ce153
Dmitry Zinushin <zinushin@gmail.com>
2024-02-01 15:17:58 +0300

    add staff

---
+++ golang/httpserver/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+func getRoot(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("got / request\n")
+	resp := fmt.Sprintf("got request method: %s host: %s  req Uri: %s \n", r.Method, r.Host, r.RequestURI)
+
+	io.WriteString(w, resp)
+}
+func getHello(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("got /hello request\n")
+	io.WriteString(w, "Hello, HTTP!\n")
+}
+
+const keyServerAddr = "serverAddr"
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", getRoot)
+	mux.HandleFunc("/hello", getHello)
+
+	err := http.ListenAndServe(":8080", mux)
+	if errors.Is(err, http.ErrServerClosed) {
+		fmt.Printf("server closed\n")
+	} else if err != nil {
+		fmt.Printf("error starting server: %s\n", err)
+		os.Exit(1)
+	}
+}
